ce77a5b8a41bb171980329faab101677a13bc93b
wtpeter <wtpeter6@gmail.com>
2026-08-21 08:22:28 +0800

    Add closed-shell TD SOC driver

--- src/nest/soc/__init__.py
+++ src/nest/soc/__init__.py
@@ -21,5 +21,6 @@
 from nest.soc.nttda import SOC as NTTDASOC
 from nest.soc.sftda import SOC as SFTDASOC
 from nest.soc.soc import SOCBase, SpinFreeState
+from nest.soc.tdrhf import SOC as TDRHFSOC
 
-__all__ = ['SOCBase', 'SpinFreeState', 'NTTDASOC', 'SFTDASOC']
+__all__ = ['SOCBase', 'SpinFreeState', 'NTTDASOC', 'SFTDASOC', 'TDRHFSOC']
